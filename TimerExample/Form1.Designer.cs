da80f4c81ee97084c8cd5985cfe402bab92e4b1b
Tony Theodoropoulos <theodot@outlook.com>
2021-01-05 10:52:09 -0500

    Add project files.

---
+++ TimerExample/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TimerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.debugLabel2 = new System.Windows.Forms.Label();
+            this.debugLabel1 = new System.Windows.Forms.Label();
+            this.colourLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timeOutput = new System.Windows.Forms.Label();
+            this.countTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(440, 44);
+            this.titleLabel.TabIndex = 16;
+            this.titleLabel.Text = "Timer Examples";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(109, 240);
+            this.startButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(91, 42);
+            this.startButton.TabIndex = 20;
+            this.startButton.Text = "Start";
+            this.startButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counterLabel.ForeColor = System.Drawing.Color.White;
+            this.counterLabel.Location = new System.Drawing.Point(212, 73);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(26, 29);
+            this.counterLabel.TabIndex = 19;
+            this.counterLabel.Text = "0";
+            // 
+            // debugLabel2
+            // 
+            this.debugLabel2.AutoSize = true;
+            this.debugLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debugLabel2.ForeColor = System.Drawing.Color.White;
+            this.debugLabel2.Location = new System.Drawing.Point(113, 124);
+            this.debugLabel2.Name = "debugLabel2";
+            this.debugLabel2.Size = new System.Drawing.Size(86, 29);
+            this.debugLabel2.TabIndex = 18;
+            this.debugLabel2.Text = "colour:";
+            // 
+            // debugLabel1
+            // 
+            this.debugLabel1.AutoSize = true;
+            this.debugLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debugLabel1.ForeColor = System.Drawing.Color.White;
+            this.debugLabel1.Location = new System.Drawing.Point(123, 73);
+            this.debugLabel1.Name = "debugLabel1";
+            this.debugLabel1.Size = new System.Drawing.Size(83, 29);
+            this.debugLabel1.TabIndex = 17;
+            this.debugLabel1.Text = "count: ";
+            // 
+            // colourLabel
+            // 
+            this.colourLabel.BackColor = System.Drawing.Color.White;
+            this.colourLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colourLabel.ForeColor = System.Drawing.Color.White;
+            this.colourLabel.Location = new System.Drawing.Point(212, 124);
+            this.colourLabel.Name = "colourLabel";
+            this.colourLabel.Size = new System.Drawing.Size(27, 30);
+            this.colourLabel.TabIndex = 21;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(41, 177);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(158, 29);
+            this.timeLabel.TabIndex = 22;
+            this.timeLabel.Text = "elapsed time:";
+            // 
+            // timeOutput
+            // 
+            this.timeOutput.AutoSize = true;
+            this.timeOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeOutput.ForeColor = System.Drawing.Color.White;
+            this.timeOutput.Location = new System.Drawing.Point(212, 177);
+            this.timeOutput.Name = "timeOutput";
+            this.timeOutput.Size = new System.Drawing.Size(26, 29);
+            this.timeOutput.TabIndex = 23;
+            this.timeOutput.Text = "0";
+            // 
+            // countTimer
+            // 
+            this.countTimer.Interval = 1000;
+            this.countTimer.Tick += new System.EventHandler(this.countTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(437, 359);
+            this.Controls.Add(this.timeOutput);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.colourLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.debugLabel2);
+            this.Controls.Add(this.debugLabel1);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label counterLabel;
+        private System.Windows.Forms.Label debugLabel2;
+        private System.Windows.Forms.Label debugLabel1;
+        private System.Windows.Forms.Label colourLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label timeOutput;
+        private System.Windows.Forms.Timer countTimer;
+    }
+}
+
---
+++ TimerExample/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TimerExample
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+
+        Stopwatch myWatch = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (countTimer.Enabled == false)
+            {
+                countTimer.Enabled = true;
+                startButton.Text = "Pause";
+
+                //myWatch.Reset();
+                myWatch.Start();
+            }
+            else
+            {
+                countTimer.Enabled = false;
+                startButton.Text = "Start";
+
+                myWatch.Stop();
+                timeOutput.Text = myWatch.Elapsed.ToString(@"ss\:ff");
+            }
+        }
+
+        private void countTimer_Tick(object sender, EventArgs e)
+        {
+            // add 1 to counter and display the value to the screen
+            counter++;
+            counterLabel.Text = $"{counter}";
+
+            // based on the value of the counter a different colour is shown
+
+            //if (counter == 1) 
+            //{
+            //    colourLabel.BackColor = Color.LightGreen;
+            //}
+            //else if (counter == 2) 
+            //{
+            //    colourLabel.BackColor = Color.DodgerBlue;
+            //}
+            //else
+            //{
+            //    colourLabel.BackColor = Color.Yellow;
+            //    counter = 0; 
+            //}
+
+            if (counter % 3 == 1)
+            {
+                colourLabel.BackColor = Color.LightGreen;
+            }
+            else if (counter % 3 == 2)
+            {
+                colourLabel.BackColor = Color.DodgerBlue;
+            }
+            else
+            {
+                colourLabel.BackColor = Color.Yellow;
+            }
+        }
+    }
+}
